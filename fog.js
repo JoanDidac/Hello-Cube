b6b3ede354c752c0db4eb94505fefd9ddf6b0f6e
Joan D <joandidachr@gmail.comm>
2023-05-11 03:19:25 +0200

    Adds groundplane depth and prepares for fog

--- fog.js
+++ fog.js
@@ -3,3 +3,12 @@ import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitCo
 export function initOrbitControls(camera, renderer) {
   let controls = new OrbitControls(camera, renderer.domElement);
 }
+
+export function initFog(scene, near, far, color, torus, camera) {
+  scene.fog = new THREE.Fog(color, near, far);
+
+  torus.addEventListener("change", () => {
+    scene.fog.near = torus.position.distanceTo(camera.position) + near;
+    scene.fog.far = torus.position.distanceTo(camera.position) + far;
+  });
+}
